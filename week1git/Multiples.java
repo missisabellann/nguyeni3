1e03d19bcd365d320ff60223191d2237478fddb7
missisabellann <isabella.nguyen@mail.utoronto.ca>
2023-09-11 15:23:36 -0400

    Upload Multiples Test

---
+++ week1git/Multiples.java
@@ -0,0 +1,11 @@
+public class Multiples {
+    public static int main(int n; int a; int b) {
+        int count = 0;
+        for(int i=1; i<n; i++){
+            if ((i%a == 0) || (i%b == 0)){
+                count++;
+            }
+        }
+        return count
+    }
+}
