68b6d4785736bebe09138400da0e3ae7a65c75df
missisabellann <isabella.nguyen@mail.utoronto.ca>
2023-09-11 15:42:16 -0400

    upload reduce

---
+++ week1git/Reduce.java
@@ -0,0 +1,14 @@
+public class Reduce {
+    public static int (int n) {
+        int count = 0;
+        while (n>0){
+            if (n%2 == 0){
+                n = n/2;
+            } else {
+                n-=1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
