ea67471897846f1617e0f8ce34bb0b56fcfbde8c
missisabellann <isabella.nguyen@mail.utoronto.ca>
2023-09-12 12:13:53 -0400

    fixed parameter syntax

--- week1git/Multiples.java
+++ week1git/Multiples.java
@@ -1,11 +1,21 @@
 public class Multiples {
-    public static int main(int n; int a; int b) {
+    public static int main(int n, int a, int b) {
         int count = 0;
         for(int i=1; i<n; i++){
             if ((i%a == 0) || (i%b == 0)){
                 count++;
             }
         }
-        return count
+        return count;
+    }
+
+    public static int fixedMultiples(int n=1000, int a=3, int b = 5){
+        int count = 0;
+        for(int i=1; i<n; i++){
+            if ((i%a == 0) || (i%b == 0)){
+                count++;
+            }
+        }
+        return count;
     }
 }
