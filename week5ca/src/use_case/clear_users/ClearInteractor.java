f0c71feaf8c2f188a229ab601c0092e0b31cf618
missisabellann <isabella.nguyen@mail.utoronto.ca>
2023-10-22 15:51:43 -0400

    completed assignment

--- week5ca/src/use_case/clear_users/ClearInteractor.java
+++ week5ca/src/use_case/clear_users/ClearInteractor.java
@@ -5,23 +5,23 @@ package use_case.clear_users;
 
 import data_access.FileUserDataAccessObject;
 import interface_adapter.clear_users.ClearPresenter;
+import use_case.signup.SignupUserDataAccessInterface;
 
 import java.util.ArrayList;
 
 public class ClearInteractor implements ClearInputBoundary{
     final ClearOutputBoundary clearPresenter;
+    final ClearUserDataAccessInterface userDataAccessObject;
 
-    final FileUserDataAccessObject clearUserDataAccessObject;
-
-    public ClearInteractor(FileUserDataAccessObject clearUserDataAccessObject,
+    public ClearInteractor(ClearUserDataAccessInterface userDataAccessObject,
                            ClearOutputBoundary clearOutputBoundary) {
         this.clearPresenter = clearOutputBoundary;
-        this.clearUserDataAccessObject = clearUserDataAccessObject;
+        this.userDataAccessObject = userDataAccessObject;
     }
     @Override
     public void execute() {
         try {
-            ArrayList<String> deleted = clearUserDataAccessObject.delete();
+            ArrayList<String> deleted = userDataAccessObject.delete();
             // Notify the output boundary of success
             ClearOutputData clearOutputData = new ClearOutputData(deleted);
             clearPresenter.prepareSuccessView(clearOutputData);
