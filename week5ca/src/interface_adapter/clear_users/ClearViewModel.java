87a0a955dff6b09db8b8adabef8f3ded7f32e410
missisabellann <isabella.nguyen@mail.utoronto.ca>
2023-10-22 16:07:46 -0400

    completed assignment

--- week5ca/src/interface_adapter/clear_users/ClearViewModel.java
+++ week5ca/src/interface_adapter/clear_users/ClearViewModel.java
@@ -1,9 +1,6 @@
 package interface_adapter.clear_users;
 
-// TODO Complete me
-
 import interface_adapter.ViewModel;
-import interface_adapter.login.LoginState;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
--- week5ca/src/use_case/clear_users/ClearInteractor.java
+++ week5ca/src/use_case/clear_users/ClearInteractor.java
@@ -1,12 +1,4 @@
 package use_case.clear_users;
-
-
-
-
-import data_access.FileUserDataAccessObject;
-import interface_adapter.clear_users.ClearPresenter;
-import use_case.signup.SignupUserDataAccessInterface;
-
 import java.util.ArrayList;
 
 public class ClearInteractor implements ClearInputBoundary{
